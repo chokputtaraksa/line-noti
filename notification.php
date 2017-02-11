245be726e086b8620d3424ee4b1082a5c98403a1
whitedevilza <darkness_za@hotmail.co.th>
2017-02-11 17:35:21 +0700

    first commit

--- notification.php
+++ notification.php
@@ -1,6 +1,6 @@
 <?php
   require_once './vendor/autoload.php';
-  $token = 'LINE_NOTIFY_TOKEN';
+  $token = 'EASUotBsecFW6aYjDpKhsLAyV4jamXgXd5bx0kQR0nY';
   $ln = new KS\Line\LineNotify($token);
   $text = 'สวัสดี Line Notify';
   $ln->send($text);
