9780ef592fe53df2a6bd5f04a0072ec332747bbd
whitedevilza <darkness_za@hotmail.co.th>
2017-02-11 21:23:39 +0700

    test ja

--- notification.php
+++ notification.php
@@ -1,6 +1,10 @@
 <?php
   require_once './vendor/autoload.php';
   $token = 'EASUotBsecFW6aYjDpKhsLAyV4jamXgXd5bx0kQR0nY';
+
   $ln = new KS\Line\LineNotify($token);
-  $text = 'สวัสดี Line Notify';
-  $ln->send($text);
+  function writeMsg($message) {
+    $ln->send($message);
+  }
+
+  writeMsg('hello world~~!!');
